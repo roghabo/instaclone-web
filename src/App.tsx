49da0b1cfb13e37c66d71476178c6b77800fd068
Jinyoung <roghabo@gmail.com>
2021-07-18 18:08:58 +0900

    to 9.1

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,28 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
+import { useReactiveVar } from "@apollo/client";
+import { darkModeVar, isLoggedInVar } from "./apollo";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, GlobalStyles, lightTheme } from "./styles";
 
 function App() {
-  const isLoggedIn = true;
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const darkMode = useReactiveVar(darkModeVar);
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          {isLoggedIn ? <Home /> : <Login />}
-        </Route>
-        <Route>
-          <NotFound />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            {isLoggedIn ? <Home /> : <Login />}
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 
-const Login = () => {
-  return <h1>Login</h1>;
-};
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.fontColor};
+`;
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+function Login() {
+  return (
+    <Container>
+      <Title>Login</Title>
+    </Container>
+  );
+}
 export default Login;
